ad48d587c0d1c9d5fba6b6620d6a0e3e328e3f1a
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-08-14 19:41:18 -0300

    Primeiro Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "Meu Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <head>
+        <link rel="shortcut icon" href="logo.ico" type="image/x-icon" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
