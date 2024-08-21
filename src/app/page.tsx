9a6a90562825a5b520cbf7a2dcc5c9496d936071
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-08-21 17:14:36 -0300

    adicionando footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Header from "./components/header";
 import Hero from "./components/hero";
-
+import Rodape from "./components/rodape";
 
 
 export default function Home() {
   return (
-    <>
+    <div className="block">
     <Header />
     <Hero />
-    </>
+   <Rodape />
+    </div>
   );
 }
