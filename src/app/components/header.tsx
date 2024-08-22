e1157de5d8e2f0e4f24a47f25e2abf472a8951ca
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-08-21 21:01:32 -0300

    adicionando hamburger icon

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,28 +1,34 @@
 "use client"
 import React from "react"
 import Logo from "./ui/logo";
-import { Avatar } from "@nextui-org/react";
-export type NavigationType ={
-    name: string;
-    href: string;
-    id: number
-}
-
-const navigation: NavigationType[]=[
-    {name: "Home", href:"#", id:1},
-    {name: "Projetos", href:"#", id:1},
-    {name: "Skills", href:"#", id:1},
-    {name: "Contatos", href:"#", id:1},
-];
+import { AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const handleNav =() =>{
+        setMenuOpen(!menuOpen);
+    }
     return (
-          <div className="bg-green-400">
-            <header>
-                <Logo />
-                <nav>
-                    
-                </nav>
-            </header>
-          </div>      
+          <nav className="fixed w-full h-24 bg-green-900 text-white shadow-2xl" >
+            <div className="flex justify-between items-center h-full px-4 2xl:px-16">
+            <Logo /> 
+                <div >
+                    <ul className="hidden sm:flex">
+                        <li>
+                            <a href="#home" className="ml-10 uppercase hover:border-b text-xl">Home</a>
+                            </li>
+                        <li>
+                            <a href="#projects" className="ml-10 uppercase hover:border-b text-xl">Projetos</a>
+                            </li>
+                        <li>
+                            <a href="#skills"  className="ml-10 uppercase hover:border-b text-xl">Skills</a>
+                            </li>
+                </ul>
+                </div>
+                <div onClick={handleNav} className=" md:hidden cursor-pointer pl-24">
+                    <AiOutlineMenu size={25} />
+                </div>
+            </div>
+          </nav>      
         )
 };
