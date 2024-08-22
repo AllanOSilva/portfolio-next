637ec26c18bc644d29445d401b876e066417ee68
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-08-21 21:37:27 -0300

    navbar atualizada

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from "react"
 import Logo from "./ui/logo";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu,  AiOutlineClose } from "react-icons/ai";
 import { useState } from "react";
 export default function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -12,8 +12,8 @@ export default function Header() {
           <nav className="fixed w-full h-24 bg-green-900 text-white shadow-2xl" >
             <div className="flex justify-between items-center h-full px-4 2xl:px-16">
             <Logo /> 
-                <div >
-                    <ul className="hidden sm:flex">
+                <div className="hidden sm:flex px-4">
+                    <ul className="flex">
                         <li>
                             <a href="#home" className="ml-10 uppercase hover:border-b text-xl">Home</a>
                             </li>
@@ -27,6 +27,31 @@ export default function Header() {
                 </div>
                 <div onClick={handleNav} className=" md:hidden cursor-pointer pl-24">
                     <AiOutlineMenu size={25} />
+                </div>
+            </div>
+            <div className={
+                menuOpen
+                ? "fixed-0 top-0 w-[65%] sm:hidden h-screen bg-green-700 text-white p-10 ease-in duration-500" 
+                :"fixed left-[-100%] p-10 ease-in duration-500"
+            }
+            >
+                <div className="flex w-full items-center justify-center">
+                <div onClick={handleNav} className=" md:hidden cursor-pointer pl-24">
+                    <AiOutlineClose size={25} />
+                </div>
+                </div>
+                <div className="flex-col py-4">
+                    <ul>
+                        <li onClick={()=>setMenuOpen(false)} className="py-4 cursor-pointer">
+                            <a href="#home">Home</a>
+                            </li>
+                        <li onClick={()=>setMenuOpen(false)} className="py-4 cursor-pointer">
+                            <a href="#projects">Projetos</a>
+                            </li>
+                        <li onClick={()=>setMenuOpen(false)} className="py-4 cursor-pointer">
+                            <a href="#skills">Skills</a>
+                            </li>
+                    </ul>
                 </div>
             </div>
           </nav>      
