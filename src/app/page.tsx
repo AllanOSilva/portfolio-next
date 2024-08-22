862c6d50b80a88dd8cf7d9979813ff1537a57a29
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-08-22 00:29:52 -0300

    colocando ids

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,11 @@ import Rodape from "./components/rodape";
 export default function Home() {
   return (
     <div className="block">
+      <section id="home"></section>
     <Header />
     <Hero />
+    <section id="projects">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eaque inventore ratione aperiam dignissimos laborum est impedit dolorum quam molestiae accusantium earum saepe, ad esse dolores tenetur recusandae voluptatibus repellat.</section>
+    <section id="skills">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto porro similique doloribus, exercitationem obcaecati officia deleniti recusandae sed mollitia, voluptatibus ullam. Sed, nulla ipsum saepe eum ducimus nostrum iusto tenetur!</section>
    <Rodape />
     </div>
   );
